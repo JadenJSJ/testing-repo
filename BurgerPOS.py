d57f190f96d2639e7b55b3ac29f1985064232a0e
JadenJSJ <jadenjsj@proton.me>
2024-10-12 12:30:53 +0700

    add new McFish menu item & make printouts more dynamic

--- BurgerPOS.py
+++ BurgerPOS.py
@@ -3,23 +3,21 @@
 # JadenJSJ
 
 def debug(text):
-	print('\33[90m' + "[debug] " + str(text) + '\33[0m')
+    print('\33[90m' + "[debug] " + str(text) + '\33[0m')
 
-burger = [["McChicken",1.20,0],["McBeef",1.40,0],["McCheese",1.00,0]]
+burger = [["McChicken",1.20,0],["McBeef",1.40,0],["McCheese",1.00,0],["McFish",1.60,0]]
 
 def FunBurger():
-    print("===================\n█ ▀█▀ █▀▀ █▀▄▀█ █▀\n█ ░█░ ██▄ █░▀░█ ▄█\n===================\n1. McChicken\t$1.20\n2. McBeef\t$1.40\n3. McCheese\t$1.00\n")
+    print("===================\n█ ▀█▀ █▀▀ █▀▄▀█ █▀\n█ ░█░ ██▄ █░▀░█ ▄█\n===================")
+    for i in range(len(burger)):
+        print(str(i+1) + ". " + burger[i][0] + "\t$" + f'{burger[i][1]:.2f}')
     itemChoise = int(input("Choise: "))
     itemQuantity = int(input("Quantity: "))
 
-    if itemChoise == 1:
-        burger[0][2] = burger[0][2] + itemQuantity
-    
-    elif itemChoise == 2:
-        burger[1][2] = burger[1][2] + itemQuantity
-
-    elif itemChoise == 3:
-        burger[2][2] = burger[2][2] + itemQuantity
+    for i in range(len(burger)):
+        if itemChoise == i+1:
+            burger[i][2] = burger[i][2] + itemQuantity
+            break
 
     main()
 
@@ -27,14 +25,10 @@ def FunCheckout():
     print("---------------------------------------------------")
     print(f'{"Item":12}{"Price":12}{"Quantity":16}{"Total":18}')
     print("---------------------------------------------------")
-    if not burger[0][2] == 0:
-        print(f'{burger[0][0]:12}{burger[0][1]:.2f}{burger[0][2]:16}{burger[0][1]*burger[0][2]:18.2f}')
 
-    if not burger[1][2] == 0:
-        print(f'{burger[1][0]:12}{burger[1][1]:.2f}{burger[1][2]:16}{burger[1][1]*burger[1][2]:18.2f}')
-
-    if not burger[2][2] == 0:
-        print(f'{burger[2][0]:12}{burger[2][1]:.2f}{burger[2][2]:16}{burger[2][1]*burger[2][2]:18.2f}')
+    for i in range(len(burger)):
+        if not burger[i][2] == 0:
+            print(f'{burger[i][0]:12}{burger[i][1]:.2f}{burger[i][2]:16}{burger[i][1]*burger[i][2]:18.2f}')
 
     print("---------------------------------------------------")
     print("Total Purchase: " + f'{burger[0][1]*burger[0][2] + burger[1][1]*burger[1][2] + burger[2][1]*burger[2][2]:.2f}')
